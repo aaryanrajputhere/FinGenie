0324f22255323ff4892ca97cc070effddc64f1f3
Aaryan Rajput <119790249+aaryanrajputhere@users.noreply.github.com>
2025-05-08 13:57:39 +0530

    Home Fixes4

--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -2,12 +2,7 @@
 import Chat from "../components/Chat";
 
 // Define the user type
-interface User {
-  id: string;
-  name: string;
-  email: string;
-  createdAt: string;
-}
+
 
 export default function Home() {
 
--- web/src/pages/transactions.tsx
+++ web/src/pages/transactions.tsx
@@ -1,10 +1,8 @@
-import Header from "../components/Header";
 import Transactions from "../components/Transactions";
 
 export default function Transaction(){
     return (
         <div>
-            <Header/>
             <Transactions/>
         </div>
     )
