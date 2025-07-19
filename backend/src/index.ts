ab419d301c9c97d571286e555a0424f3f0ae3d1c
aaryanrajputhere <itsaaryanrajput@gmail.com>
2025-07-19 14:03:51 +0530

    CORS fix added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,14 +5,22 @@ import userRouter from './routes/user'
 import cors from "cors"
 const app = express();
 app.use(bodyParser.json());
+
 const allowedOrigins = ['https://fingenieai.vercel.app'];
 
 app.use(cors({
-  origin: allowedOrigins,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
 app.use('/api/v1/expense', expenseRouter);
 app.use('/api/v1/user' , userRouter)
 const PORT = process.env.PORT || 3000;
